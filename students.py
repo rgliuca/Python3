a02c161b8b58d9bae19f05043d0855de54157628
rgliuca <44307228+rgliuca@users.noreply.github.com>
2020-08-03 06:07:20 -0700

    Add files via upload

---
+++ students.py
@@ -0,0 +1,13 @@
+students=[ {'name':'Tommy',
+            'homework':[70,80,88,79],
+            'quiz':[88,78,50],
+            'test':[75,68]},
+           {'name':'Alex',
+            'homework':[90,98,87,97],
+            'quiz':[95,89,91],
+            'test':[88,90]},
+           {'name':'Jason',
+            'homework':[50,89,71,82],
+            'quiz':[71,87,70],
+            'test':[78,81]}]
+
