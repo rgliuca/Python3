ffad7fe187ba491b3a8f0e8e99f6f581134536af
rgliuca <44307228+rgliuca@users.noreply.github.com>
2020-08-07 21:28:03 -0700

    Update students.py

--- students.py
+++ students.py
@@ -1,5 +1,5 @@
 students=[ {'name':'Tommy',
-            'homework':[70,80,88,79],
+            'homework':[62,80,88,79],
             'quiz':[88,78,50],
             'test':[75,68]},
            {'name':'Alex',
@@ -7,7 +7,7 @@ students=[ {'name':'Tommy',
             'quiz':[95,89,91],
             'test':[88,90]},
            {'name':'Jason',
-            'homework':[50,89,71,82],
+            'homework':[62,89,71,82],
             'quiz':[71,87,70],
             'test':[78,81]}]
 
